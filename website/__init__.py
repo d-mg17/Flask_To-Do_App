1ad05ae5b2355889fb36addc602ec9149077c2e4
Diego Martinez <d.mg17@outlook.com>
2023-01-10 17:50:14 -0400

    Fixed error, typo.

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,6 @@ def createToDoApp():
     from .auth import auth
     
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth/")
+    app.register_blueprint(auth, url_prefix="/")
     
     return app
