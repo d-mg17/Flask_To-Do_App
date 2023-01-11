8207b2db3d5ef6e2b47d5d49e3ccfcc393fb0d94
Diego Martinez <d.mg17@outlook.com>
2023-01-10 20:10:53 -0400

    Update in auth, views, and html files

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,43 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, request, flash
 
-auth = Blueprint("auth",__name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "<h3>Log in</h3>"
+    # data = request.form
+    # print(data)
+    return render_template("login.html")
 
-@auth.route('/logout')
+
+@auth.route("/logout")
 def logout():
-    return "<h3>Logging Out</h3>"
+    return redirect("/")
 
-@auth.route('/signup')
+
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
-    return "<h3>Sign-Up</h3>"
+    if request.method == "POST":
+
+        email = request.form.get(key="email")
+        username = request.form.get(key="username")
+        password1 = request.form.get(key="password")
+        password2 = request.form.get(key="password-confirmation")
+
+        if len(email) < 7:
+            flash("Email too short. Enter a valid email.", category="error")
+        elif len(username) < 2:
+            flash(
+                "Username too short, must be at least 2 characters.", category="error"
+            )
+        elif len(password1) < 7:
+            flash(
+                "Password too short, must be at least 8 characters.", category="error"
+            )
+        elif password1 != password2:
+            flash("Passwords don't match.", category="error")
+        else:
+            flash("Creating account...", category="success")
+            # Add user to database
+            pass
+    return render_template("signup.html")
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, redirect, render_template)
+from flask import (Blueprint, render_template)
 
 views = Blueprint("views",__name__)
 
